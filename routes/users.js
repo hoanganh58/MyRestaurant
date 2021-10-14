1578b608f35c725fd3e961e34bbd7d43072f87b4
Hoang Anh <anhcaca123@gmail.com>
2021-10-14 11:57:32 +0700

    main

--- routes/users.js
+++ routes/users.js
@@ -53,7 +53,7 @@ router.post('/register', checkFormRegister ,function(req, res, next) {
           email: email,
           username: username,
           password: password,
-          admin: 1
+          admin: 0
         });
         bcrypt.genSalt(10, function(err, salt) {
           bcrypt.hash(user.password, salt, function(err, hash){
